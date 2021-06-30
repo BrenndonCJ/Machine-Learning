25c93362c75915e62b40b723c72680b96f06475b
DEV-BrenndonCJ <74077987+DEV-BrenndonCJ@users.noreply.github.com>
2021-06-30 12:06:22 -0300

    Analise Tweets Desastres

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\BrenndonCJ\Desktop\DEV\Projetcts and Datas\Machine Learning\venv
+set VIRTUAL_ENV=C:\Users\BrenndonCJ\Documents\GitHubProjects\Machine-Learning\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
